e76fa25cf0c533842c0730a840a0ff82975ec929
Angel Ponce <j.angelponce.esc@gmail.com>
2021-08-07 22:35:51 -0600

    Adding devise and generating user and admin models

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "=== Running seeds ==="
+user_params = { email: "user@test.com", password: "P@ssw0rd" }
+admin_params = { email: "admin@test.com", password: "P@ssw0rd" }
+
+user = User.new(user_params)
+if user.save
+  puts "User generated\n\temail: #{user_params[:email]}\n\t pass: #{user_params[:password]}"
+end
+
+admin = Admin.new(admin_params)
+if admin.save
+  puts "Admin generated\n\temail: #{admin_params[:email]}\n\t pass: #{admin_params[:password]}"
+end
